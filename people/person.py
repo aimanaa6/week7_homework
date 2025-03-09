a6803ae0e8349b1d38cb7424b093689cb8ea9849
Aiman Ahmed <aimanahmed61@gmail.com>
2025-03-09 23:39:10 +0000

    EDIT: tested getter and setter methods

--- people/person.py
+++ people/person.py
@@ -1,10 +1,10 @@
 class Person:
-    def __init__(self,firstname,lastname,age, occupation, depositamount):
+    def __init__(self,firstname,lastname,age, occupation):
         self.__firstname= firstname
         self.__lastname = lastname
         self.__age = age
         self.__occupation = occupation
-        self.__depositamount = depositamount
+
 
     def get_first_name(self):
         return self.__firstname
@@ -29,8 +29,8 @@ class Employee:
         self.__firstname = firstname
         self.__lastname = lastname
         self.__role = role
-        self.__annualsalary = annualsalary
-        self.__serviceyears = serviceyears
+        self._annualsalary = annualsalary
+        self._serviceyears = serviceyears
 
     def get_first_name(self):
         return self.__firstname
@@ -42,22 +42,13 @@ class Employee:
         return self.__role
 
     def get_service_years(self):
-        return self.__serviceyears
+        return self._serviceyears
 
     def add_service_years(self,amount):
-        self.__serviceyears = self.__serviceyears + amount
+        self._serviceyears = self._serviceyears + amount
 
     def __str__(self):
-            return f"Employee: {self.__firstname} {self.__lastname}.\n{self.__firstname} has worked here for {self.__serviceyears} years as a {self.__role}."
-
-new_employee = Employee('Prableen','Gujral','Research Assistant','Full-time','3')
-print(new_employee)
-
-# new_employee.display_role()
-# print(new_employee.display_role())
-
-# new_employee.add_service_years()
-# print(new_employee)
+            return f"Employee: {self.__firstname} {self.__lastname}.\n{self.__firstname} has worked here for {self._serviceyears} years as a {self.__role}."
 
 class Customer:
     def __init__(self,fullname,age,dataprotectionagreement,localbranch,withdrawallimit):
@@ -77,12 +68,5 @@ class Customer:
         return self.__fullname
 
     def __str__(self):
-        return (f"New Customer Name: {self.__fullname}\nThe customer is {self.__age} years old.\nDoes the customer agree to the data protection policy? {new_customer.answer_dataprotectionagreement('Y')PR}."
+        return (f"New Customer Name: {self.__fullname}\nThe customer is {self.__age} years old.\nDoes the customer agree to the data protection policy? {new_customer.answer_dataprotectionagreement('Y')}."
                 f"\n{self.__fullname}'s local branch is {self.__localbranch}. Their max withdrawal limit is {self.__withdrawallimit}")
-
-new_customer = Customer('Mark Baldock','45','Y' ,'Hounslow', '2000')
-print(new_customer)
-
-new_customer.answer_dataprotectionagreement('Y')
-print(new_customer.answer_dataprotectionagreement('Y'))
-print(new_customer.get_fullname())
--- people/profile.py
+++ people/profile.py
@@ -1,7 +1,9 @@
 from people.person import Person
+from people.person import Employee
+from people.person import Customer
 
 # person
-aiman_profile = Person('Aiman','Ahmed',23,'British','Operations Specialist')
+aiman_profile = Person('Aiman','Ahmed',23,'Operations Specialist')
 print(aiman_profile)
 
 aiman_profile.get_first_name()
@@ -15,3 +17,21 @@ print(aiman_profile.get_age())
 
 aiman_profile.set_age(18)
 print(aiman_profile.get_age())
+
+# employee
+new_employee = Employee('Prableen','Gujral','Research Assistant','Full-time',3)
+print(new_employee)
+
+new_employee.display_role()
+print(new_employee.display_role())
+
+new_employee.add_service_years(2)
+print(new_employee)
+
+# customer
+new_customer = Customer('Mark Baldock','45','Y' ,'Hounslow', '2000')
+print(new_customer)
+
+new_customer.answer_dataprotectionagreement('Y')
+print(new_customer.answer_dataprotectionagreement('Y'))
+print(new_customer.get_fullname())
